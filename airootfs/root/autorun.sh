9fa96bc1b5ea2bc95c97f54b5deda3f16d75e38e
Andrei Ciocan <andreycsc@gmail.com>
2017-05-26 23:02:30 +0300

    Auto run installer on boot

---
+++ airootfs/root/autorun.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# wait for the network to come up
+while true
+do
+  ping -c1 github.com &> /dev/null && break
+done
+
+# copy the ssh key used to authenticate to the repo
+#cp -r ./rootfs-private/home/chendry/.ssh .
+#chmod 600 .ssh/*
+
+# clone the repository
+git clone https://github.com/andreycsc/Arch_install.git
+
+# run each installation script in sequence, logging the results
+mkdir logs
+cd Arch_install/install-scripts
+for i in *.sh
+do
+  echo $i
+  bash -x $i &>> ~/logs/${i%.sh}.out
+done
