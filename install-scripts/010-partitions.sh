c0cfee5607f3b4e4fa68b1e41a6dcac7ed6aa7a7
Andrei Ciocan <andrei.ciocan@prooptica.ro>
2017-05-30 15:39:18 +0300

    Revert "by label"
    This reverts commit 21e534f7e2783b51cb548b79ea85fb1162bcf627.

--- install-scripts/010-partitions.sh
+++ install-scripts/010-partitions.sh
@@ -4,27 +4,27 @@
 #ROOT_DEV=$( blkid -L ROOT )
 #HOME_DEV=$( blkid -L HOME )
 
-if [[ -e /dev/disk/by-label/ROOT ]]; then
+if [[ -e /dev/disk/by-partlabel/ROOT ]]; then
 	# format root
-	mkfs.ext4 -F /dev/disk/by-label/ROOT
+	mkfs.ext4 -F /dev/disk/by-partlabel/ROOT
 	# mount root
 	mkdir -p /mnt
-	mount /dev/disk/by-label/ROOT /mnt
+	mount /dev/disk/by-partlabel/ROOT /mnt
 fi
 
 #if home not defined wil not be on separate partition
-if [[ -e /dev/disk/by-label/HOME ]]; then
+if [[ -e /dev/disk/by-partlabel/HOME ]]; then
 	## format home
-	mkfs.ext4 -F /dev/disk/by-label/HOME
+	mkfs.ext4 -F /dev/disk/by-partlabel/HOME
 	## Mount the home drive
 	mkdir -p /mnt/home
-	mount /dev/disk/by-label/HOME /mnt/home
+	mount /dev/disk/by-partlabel/HOME /mnt/home
 fi
 
-if [[ -e /dev/disk/by-label/BOOT ]]; then
+if [[ -e /dev/disk/by-partlabel/BOOT ]]; then
 	# format boot
-	mkfs.vfat -F32 /dev/disk/by-label/BOOT
+	mkfs.vfat -F32 /dev/disk/by-partlabel/BOOT
 	# mount boot
 	mkdir -p /mnt/boot
-	mount /dev/disk/by-label/BOOT /mnt/boot
+	mount /dev/disk/by-partlabel/BOOT /mnt/boot
 fi
